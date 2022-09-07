5b9304002cb340059c43dac8d7958894e45b41aa
rashadmarkz <rashad.markcreative.com>
2022-09-07 13:26:43 +0530

    App-Calculator Template-View Function-Calc Urls Template HTML

--- calculator/views.py
+++ calculator/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.views.generic import TemplateView
+from .forms import HomeForm
 
 # Create your views here.
+class HomePage(TemplateView):
+    template_name = 'templatesviews/home.html'
+    #template_name = 'aboutus.html'
+
+
+    #def get(self, request, *args, **kwargs):
+        #return render(request, "aboutus.html")
+
+    def get(self, request, *args, **kwargs):
+        form = HomeForm()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = HomeForm(request.POST)
+        if form.is_valid():
+            text = form.cleaned_data['num1']
+            text2 = form.cleaned_data['num2']
+            if 'add' in request.POST:
+                result = text + text2
+            elif 'sub' in request.POST:
+                result = text - text2
+            elif 'mul' in request.POST:
+                result = text * text2
+            elif 'div' in request.POST:
+                result = text / text2
+
+            form = HomeForm()
+            # return redirect ('home:home')
+
+        args = {'form': form, 'result': result}
+        return render(request, self.template_name, args)
